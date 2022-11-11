dc9aa1ef7f6dffa43a1adba96e086e0aa97864b9
tonytruong101 <ttruong@rennie.com>
2022-11-11 12:04:10 -0800

    add: initial commit of nextjs-nginx docker setup

---
+++ .next/static/PxpPP9rw-sq3DIq_EWGgc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b651ec725cfcff2c.js"],"/_error":["static/chunks/pages/_error-c829feec71ce8a09.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
